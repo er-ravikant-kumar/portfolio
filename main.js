9c5a58ee52a18709aabb819db74f7a7bb553357b
RAVIKANT KUMAR <143511865+er-ravikant-kumar@users.noreply.github.com>
2024-05-18 13:36:16 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+//NAVIGATION BAR EFFECTS ON SCROLL
+window.addEventListener("scroll",function(){
+    const header = document.querySelector("header");
+    header.classList.toggle("sticky", window.scrollY > 0);
+});
+
+var icon = document.getElementById("icon");
+icon.onclick = function(){
+    document.body.classList.toggle("dark-theme");
+    
+    if(document.body.classList.contains("dark-theme")){
+        icon.src = "sun.png";
+    }else{
+        icon.src = "moon.png";
+    }
+    
+}
